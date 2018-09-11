d8b17a2229dd59f20642785dbe759f7853a36fd1
MarkusJacobsen <markusja@stud.ntnu.no>
2018-09-11 13:17:18 +0200

    GLIDE-17 #comment Firebase project created. SDK initialized in code. #time 45m

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-"fmt"
-"github.com/Sirupsen/logrus"
-"github.com/gorilla/mux"
-"net/http"
+	"firebase.google.com/go"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
+	"log"
+	"net/http"
 )
 
 func main() {
+	initializeFirebase()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", startPage)
@@ -18,4 +24,12 @@ func main() {
 //Redirect here is url: localhost:8080 is supplied
 func startPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from go - Gliding tracks\n")
+}
+
+func initializeFirebase() {
+	opt := option.WithCredentialsFile("gt-backend-8b9c2-firebase-adminsdk-0t965-80679b9b72.json")
+	/*app*/ _, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
 }
