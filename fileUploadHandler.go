e149807017444c753f337b2520915e2b33528d2e
MarkusJacobsen <markusja@stud.ntnu.no>
2018-09-13 14:04:05 +0200

    GLIDE-19 GLIDE-48 #start-progress #comment Endpoint and func for uploading file, with type check #time 2h

---
+++ fileUploadHandler.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"github.com/Sirupsen/logrus"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// Actual processing of the file upload
+// Inspiration: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/04.5.html
+func ProcessUploadRequest(r* http.Request, uid string) (error, int) {
+	r.ParseMultipartForm(32 << 20)
+
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		logrus.Error(err)
+		return err, http.StatusBadRequest
+	}
+
+	defer file.Close()
+
+	err = checkFileContentType(file, handler)
+	if err != nil {
+		return err, http.StatusUnsupportedMediaType
+	}
+
+	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		logrus.Error(err)
+		return err, http.StatusBadRequest
+	}
+	defer f.Close()
+
+	//io.Copy(f, file)
+
+	return nil, 0
+}
+
+// Check whether or not a file is of type IGC
+// https://golang.org/pkg/net/http/#DetectContentType
+func checkFileContentType(file multipart.File, handler* multipart.FileHeader) (error){
+	buff := make([]byte, 512)
+
+	if _, err := file.Read(buff); err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	content := http.DetectContentType(buff)
+
+	if !strings.Contains(handler.Filename, ".igc") || !strings.Contains(content, "text/plain") {
+		return errors.New("invalid content-type")
+
+	}
+
+	return nil
+}
+
+func saveFileToFileSystem() {
+	// TODO save to Firebase
+}
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-"fmt"
-"github.com/Sirupsen/logrus"
-"github.com/gorilla/mux"
-"net/http"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", startPage)
+	r.HandleFunc("/upload", uploadFilePage).Methods("POST")
 
 	logrus.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -18,4 +19,20 @@ func main() {
 //Redirect here is url: localhost:8080 is supplied
 func startPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from go - Gliding tracks\n")
+}
+
+// Upload and save a file to the filesystem
+func uploadFilePage(w http.ResponseWriter, r *http.Request) {
+	uid := r.FormValue("uid")
+	if uid == "" {
+		logrus.Error("No uid supplied in request")
+		http.Error(w, "No uid supplied in request",  http.StatusBadRequest)
+		return
+	}
+
+	err, httpCode := ProcessUploadRequest(r, uid)
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, err.Error(), httpCode)
+	}
 }
