924d28480a3ac6f8d0e26d0fedd0be67d1d6660b
Markus Jacobsen <markusja@stud.ntnu.no>
2018-09-21 14:50:16 +0200

    Feature/glide 4 (#3)
    * Auto stash before merge of "develop" and "origin/develop"
    
    * GLIDE-19 GLIDE-48 #start-progress #comment Endpoint and func for uploading file, with type check #time 2h
    
    * GLIDE-4 #time 30m WIP testing fileUploadHandler
    
    * GLIDE-4 #time 2h #comment unit tests for fileupload methods
    
    * GLIDE-20 #start-progress #time 1h #comment Uploaded files are saved to .Records/{uid}/
    
    * GLIDE-4 #time 5m Clean up user filepath to only include filename
    
    * GLIDE-4 #time 2m Golint and GoFmt
    
    * GLIDE-4 Forgot to update tests after GoLint
    
    * GLIDE-4 Remaining from upstream merge. refactored routes and bind
    
    * GLIDE-4 #time 30m Dev-type terminal output. Hard-coded strings -> consts
    
    * GLIDE-4 FileUploadHandler -> named func returns
    
    * GLIDE-4 #time 1h #comment Refac storage methods to own file. Delete file
    
    * GLIDE-4 gofmt
    
    * GLIDE-4 Trailing logrus

---
+++ rest/fileUploadHandler.go
@@ -0,0 +1,102 @@
+package rest
+
+import (
+	"errors"
+	"github.com/GlidingTracks/gt-backend"
+	"github.com/GlidingTracks/gt-backend/constant"
+	"github.com/gorilla/mux"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"strings"
+)
+
+// fileNameFUH - Used in debugging. TODO remove before prod
+const fileNameFUH = "fileUploadHandler.go"
+
+// FileUploadHandler holds the context and routes for this handler.
+type FileUploadHandler struct {
+	Ctx            Context
+	UploadFilePage string
+}
+
+// Bind sets up the routes to the mux router.
+func (fuh FileUploadHandler) Bind(r *mux.Router) {
+	r.HandleFunc("/upload", uploadFilePage).Methods("POST")
+}
+
+// uploadFilePage - Upload and save a file to the filesystem
+func uploadFilePage(w http.ResponseWriter, r *http.Request) {
+	uid := r.FormValue("uid")
+	if uid == "" {
+		gtbackend.DebugLog(fileNameFUH, "uploadFilePage", errors.New(constant.ErrorNoUIDProvided))
+
+		http.Error(w, constant.ErrorNoUIDProvided, http.StatusBadRequest)
+		return
+	}
+
+	httpCode, err := ProcessUploadRequest(r, uid)
+	if err != nil {
+		gtbackend.DebugLog(fileNameFUH, "uploadFilePage", err)
+
+		http.Error(w, err.Error(), httpCode)
+	}
+}
+
+// ProcessUploadRequest - Actual processing of the file upload
+// Inspiration: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/04.5.html
+func ProcessUploadRequest(r *http.Request, uid string) (httpCode int, err error) {
+	r.ParseMultipartForm(32 << 20)
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		gtbackend.DebugLog(fileNameFUH, "ProcessUploadRequest", err)
+
+		httpCode = http.StatusBadRequest
+		return
+	}
+
+	defer file.Close()
+
+	err = checkFileContentType(file, handler)
+	if err != nil {
+		httpCode = http.StatusUnsupportedMediaType
+		return
+	}
+
+	f, err := gtbackend.SaveFileToLocalStorage(uid, handler.Filename)
+	if err != nil {
+		gtbackend.DebugLog(fileNameFUH, "ProcessUploadRequest", err)
+
+		httpCode = http.StatusBadRequest
+		return
+	}
+	defer f.Close()
+
+	io.Copy(f, file)
+
+	httpCode = http.StatusOK
+	// TODO DB entry processing comes here
+	return
+}
+
+// checkFileContentType - Check whether or not a file is of type IGC
+// https://golang.org/pkg/net/http/#DetectContentType
+func checkFileContentType(file multipart.File, handler *multipart.FileHeader) (err error) {
+	buff := make([]byte, 512)
+
+	if _, err = file.Read(buff); err != nil {
+		gtbackend.DebugLog(fileNameFUH, "checkFileContentType", err)
+
+		return
+	}
+
+	content := http.DetectContentType(buff)
+
+	if !strings.Contains(handler.Filename, "."+constant.IGCExtension) || !strings.Contains(content, constant.TextPlain) {
+		err = errors.New(constant.ErrorInvalidContentType)
+		return
+	}
+
+	return
+}
