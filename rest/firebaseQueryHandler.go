138f9590e9f11aa47a80da77a914a90c2363cf72
Markus Jacobsen <markusja@stud.ntnu.no>
2018-10-04 17:48:00 +0200

    Fixed issued from CC (#8)

--- rest/firebaseQueryHandler.go
+++ rest/firebaseQueryHandler.go
@@ -11,12 +11,13 @@ import (
 )
 
 // GetTracks gets a list of IgcMetadata from Firebase based on query
-func GetTracks(app *firebase.App, query models.FirebaseQuery) (data []models.IgcMetadata, err error){
+func GetTracks(app *firebase.App, query models.FirebaseQuery) (data []models.IgcMetadata, err error) {
 	ctx := context.Background()
 
 	client, err := app.Firestore(ctx)
 	if err != nil {
 		gtbackend.DebugLog(fileNameDB, "GetTracks", err)
+
 		return
 	}
 
@@ -25,23 +26,25 @@ func GetTracks(app *firebase.App, query models.FirebaseQuery) (data []models.Igc
 		iter := client.Collection(constant.IgcMetadata).
 			Where("UID", "==", query.UID).
 			OrderBy(query.Ord, query.OrdDir).Documents(ctx)
-		return processIterGetTracks(iter, query.Pg, "")
-	} else {
-		iter := client.Collection(constant.IgcMetadata).
-			Where("Privacy", "==", false).
-			OrderBy(query.Ord, query.OrdDir).Documents(ctx)
-		return processIterGetTracks(iter, query.Pg, query.UID)
+
+		data, err = processIterGetTracks(iter, query.Pg, "")
+		return
 	}
 
-	return data, err
+	iter := client.Collection(constant.IgcMetadata).
+		Where("Privacy", "==", false).
+		OrderBy(query.Ord, query.OrdDir).Documents(ctx)
+
+	data, err = processIterGetTracks(iter, query.Pg, query.UID)
+	return
 }
 
 /** processIterGetTracks
-	Processes the request made to Firebase based
-	iter *firestore.DocumentIterator Iterator with the results from firestore
-	pg int Page to retrieve
-	filterUID string Filter UID to remove from the results
- */
+Processes the request made to Firebase based
+iter *firestore.DocumentIterator Iterator with the results from firestore
+pg int Page to retrieve
+filterUID string Filter UID to remove from the results
+*/
 func processIterGetTracks(
 	iter *firestore.DocumentIterator,
 	pg int,
@@ -51,7 +54,7 @@ func processIterGetTracks(
 	pageItemSkip := (pg - 1) * constant.PageSize
 
 	// Process track query until length of data is the size of a page
-	for ; len(data) < constant.PageSize; {
+	for len(data) < constant.PageSize {
 		doc, err := iter.Next()
 
 		// Early break if there is no more data (last page)
