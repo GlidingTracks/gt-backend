d9eabc00841d16fb2612c8f61c8525aa6e829e32
MarkusJacobsen <markusja@stud.ntnu.no>
2018-10-04 15:57:55 +0200

    GLIDE-5 GLIDE-21 #time 15m Added privacy into request

--- common_test.go
+++ common_test.go
@@ -14,3 +14,33 @@ func TestGetUnixTime(t *testing.T) {
 		t.Error("Expected: ", expected, ", got: ", actual, "\n")
 	}
 }
+
+func TestGetBoolFromString(t *testing.T) {
+	t.Run("Return false", func(t *testing.T) {
+		b := GetBoolFromString("false")
+		if b {
+			t.Error("Expected false, got: ", b)
+		}
+	})
+
+	t.Run("Return true", func(t *testing.T) {
+		b := GetBoolFromString("true")
+		if !b {
+			t.Error("Expected true, got: ", b)
+		}
+	})
+
+	t.Run("->Empty, return false", func(t *testing.T) {
+		b := GetBoolFromString("")
+		if b {
+			t.Error("Expected false, got: ", b)
+		}
+	})
+
+	t.Run("->jibberish, return false", func(t *testing.T) {
+		b := GetBoolFromString("fewfwfewfew")
+		if b {
+			t.Error("Expected false, got: ", b)
+		}
+	})
+}
