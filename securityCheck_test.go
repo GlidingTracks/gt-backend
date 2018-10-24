0a380649c0376cadf9a0345e16b548bb6eba9250
Horcon <maciejp@stud.ntnu.no>
2018-10-24 19:23:31 +0200

    GLIDE-70 #time 1h 45m #comment Writing the test PROPERLY as it appears you have to do some serious voodoo to get authentication testing to work in tests.

--- securityCheck_test.go
+++ securityCheck_test.go
@@ -1,12 +1,16 @@
 package gtbackend
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"firebase.google.com/go"
+	"fmt"
 	"github.com/GlidingTracks/gt-backend/constant"
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
 	"google.golang.org/api/option"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -37,6 +41,13 @@ func TestSecurityMiddleware_CheckIncomingRequests(t *testing.T) {
 	}
 }
 
+type authResponse struct {
+	Kind string
+	IdToken string
+	RefreshToken string
+	ExpiresIn string
+}
+
 func RetrieveFirebaseIDToken() (app *firebase.App, token string) {
 	config := &firebase.Config{
 		StorageBucket: "gt-backend-8b9c2.appspot.com",
@@ -57,6 +68,29 @@ func RetrieveFirebaseIDToken() (app *firebase.App, token string) {
 	if err != nil {
 		logrus.Fatalf("error setting custom token: %v\n", err)
 	}
+
+	var jsonStr = []byte(`{
+	"token": "` + token + `",
+	"returnSecureToken": true
+	}`)
+	res, err := http.Post( "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyAppC_L-VHnTM1ezOvuiVCoKfFzFu6f5ZU", "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil || res.StatusCode != http.StatusOK {
+		fmt.Printf("%+v\n", string(jsonStr))
+		logrus.Fatalf("error retrieving id token: %v\n", err)
+	}
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		logrus.Fatalf("error reading body of id token retrieve req: %v\n", err)
+	}
+
+	var resParsed authResponse
+	err = json.Unmarshal(resBody, &resParsed)
+	if err != nil {
+		logrus.Fatalf("error parsing json of id token request: %v\n", err)
+	}
+
+	token = resParsed.IdToken
 
 	return
 }
